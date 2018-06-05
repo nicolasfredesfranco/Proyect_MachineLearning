f399c730c8115e6a8e91f9a8516114185a912bd3
facundolazcano <39739600+facundolazcano@users.noreply.github.com>
2018-06-05 12:09:45 -0400

    Pre prossing descarga chile

---
+++ ppsing.py
@@ -0,0 +1,52 @@
+
+from nltk import word_tokenize
+import unicodedata 
+
+
+
+abc={'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'}
+
+stop_words=['de', 'la', 'que', 'el', 'en', 'y', 'a', 'los', 'del', 'se', 'las', 'por', 'un', 'para', 'con', 'no', 'una', 'su', 'al', 'lo', 'como', 'mas', 'pero', 'sus', 'le', 'ya', 'o', 'este', 'si', 'porque', 'esta', 'entre', 'cuando', 'muy', 'sin', 'sobre', 'tambien', 'me', 'hasta', 'hay', 'donde', 'quien', 'desde', 'todo', 'nos', 'durante', 'todos', 'uno', 'les', 'ni', 'contra', 'otros', 'ese', 'eso', 'ante', 'ellos', 'e', 'esto', 'mi', 'antes', 'algunos', 'que', 'unos', 'yo', 'otro', 'otras', 'otra', 'el', 'tanto', 'esa', 'estos', 'mucho', 'quienes', 'nada', 'muchos', 'cual', 'poco', 'ella', 'estar', 'estas', 'algunas', 'algo', 'nosotros', 'mi', 'mis', 'tu', 'te', 'ti', 'tu', 'tus', 'ellas', 'nosotras', 'vosostros', 'vosostras', 'os', 'mio', 'mia', 'mios', 'mias', 'tuyo', 'tuya', 'tuyos', 'tuyas', 'suyo', 'suya', 'suyos', 'suyas', 'nuestro', 'nuestra', 'nuestros', 'nuestras', 'vuestro', 'vuestra', 'vuestros', 'vuestras', 'esos', 'esas', 'estoy', 'estas', 'esta', 'estamos', 'estais', 'estan', 'este', 'estes', 'estemos', 'esteis', 'esten', 'estare', 'estaras', 'estara', 'estaremos', 'estareis', 'estaran', 'estaria', 'estarias', 'estariamos', 'estariais', 'estarian', 'estaba', 'estabas', 'estabamos', 'estabais', 'estaban', 'estuve', 'estuviste', 'estuvo', 'estuvimos', 'estuvisteis', 'estuvieron', 'estuviera', 'estuvieras', 'estuvieramos', 'estuvierais', 'estuvieran', 'estuviese', 'estuvieses', 'estuviesemos', 'estuvieseis', 'estuviesen', 'estando', 'estado', 'estada', 'estados', 'estadas', 'estad', 'he', 'has', 'ha', 'hemos', 'habeis', 'han', 'haya', 'hayas', 'hayamos', 'hayais', 'hayan', 'habre', 'habras', 'habra', 'habremos', 'habreis', 'habran', 'habria', 'habrias', 'habriamos', 'habriais', 'habrian', 'habia', 'habias', 'habiamos', 'habiais', 'habian', 'hube', 'hubiste', 'hubo', 'hubimos', 'hubisteis', 'hubieron', 'hubiera', 'hubieras', 'hubieramos', 'hubierais', 'hubieran', 'hubiese', 'hubieses', 'hubiesemos', 'hubieseis', 'hubiesen', 'habiendo', 'habido', 'habida', 'habidos', 'habidas', 'soy', 'eres', 'es', 'somos', 'sois', 'son', 'sea', 'seas', 'seamos', 'seais', 'sean', 'sere', 'seras', 'sera', 'seremos', 'sereis', 'seran', 'seria', 'serias', 'seriamos', 'seriais', 'serian', 'era', 'eras', 'eramos', 'erais', 'eran', 'fui', 'fuiste', 'fue', 'fuimos', 'fuisteis', 'fueron', 'fuera', 'fueras', 'fueramos', 'fuerais', 'fueran', 'fuese', 'fueses', 'fuesemos', 'fueseis', 'fuesen', 'sintiendo', 'sentido', 'sentida', 'sentidos', 'sentidas', 'siente', 'sentid', 'tengo', 'tienes', 'tiene', 'tenemos', 'teneis', 'tienen', 'tenga', 'tengas', 'tengamos', 'tengais', 'tengan', 'tendre', 'tendras', 'tendra', 'tendremos', 'tendreis', 'tendran', 'tendria', 'tendrias', 'tendriamos', 'tendriais', 'tendrian', 'tenia', 'tenias', 'teniamos', 'teniais', 'tenian', 'tuve', 'tuviste', 'tuvo', 'tuvimos', 'tuvisteis', 'tuvieron', 'tuviera', 'tuvieras', 'tuvieramos', 'tuvierais', 'tuvieran', 'tuviese', 'tuvieses', 'tuviesemos', 'tuvieseis', 'tuviesen', 'teniendo', 'tenido', 'tenida', 'tenidos', 'tenidas', 'tened','asi','q','d']
+
+
+#esta funcion elmina todos los signos que no son palabras
+def elimina_sign(text):
+    for i in range(len(text)-1):
+        if text[i] not in abc:
+             text=text[:i] + ' ' +text[i + 1:]
+    if text[len(text)-1] not in abc:
+        text=text[:len(text)-1]
+    return text
+
+def elimina_tildes(s):
+   return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
+
+
+def processing(text):
+    output=list()        
+    
+    #quita el enlace
+    text=text.split('https')[0]
+
+    #pasa todo a minusculas
+    text = text.lower()
+
+    #elmina signos de puntuacion
+    text=elimina_tildes(text)
+
+    #elimina caracteres
+    text=elimina_sign(text)
+
+    #separa el texto por frases u oraciones
+    frases=text.split('.');
+
+    #separa por las palabras
+    for fr in frases:
+        if len(fr)!=0: 
+            words = word_tokenize(fr)
+            output.append(list(filter(lambda x: x not in stop_words, words)))
+
+    return output
+
+
+
---
+++ tweet2.py
@@ -0,0 +1,53 @@
+import tweepy
+import unicodedata
+import ppsing #modulos de pre procesado
+from gensim.models import Word2Vec
+from unipath import Path
+
+consumer_key = "YG9t6aMkiK3rvGdSbIkNbVKoG"
+consumer_secret = "xhyivSyaesMQ2PvJdceI7BwBX3sjXxzhHsvC2pZcV0i2VTZ16R"
+access_key = "879488168525004801-2wqBqwGmQzUNbvZLC1Oja7dyOF0ztLK"
+access_secret = "ajMqqwF7TBf2tQZpLQyNZfe9jSLh8K6USWJHjXkp1Ohwg"
+
+#configurar api
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+#ubicar chile    
+places = api.geo_search(query="CHILE", granularity="country")
+place_id = places[0].id
+
+
+def get_all_tweets(num_tweets):
+
+    #salida, lista de listas de frases
+    alltweets = list()    
+
+    #new_tweets= api.user_timeline(screen_name = screen_name,count=1,include_rts=False, tweet_mode="extended")
+
+    counter=0
+    while (counter<num_tweets):
+        
+        new_tweets = api.search(q="place:%s" % place_id,tweet_mode='extended')
+        
+        if len(new_tweets) != 0:
+
+            #guarda el id del ultimo
+            oldest=new_tweets[-1].id-1 
+            
+            #por cada iteracion agrega las frases nuevas del tweets
+            alltweets.extend(ppsing.processing(new_tweets[0].full_text))
+
+            counter += 1
+    print(new_tweets[0].full_text)
+    return alltweets
+
+if __name__ == '__main__':
+
+
+    num_tweets=int(input('ingrese cantidad de tweets:'))
+    tweets=get_all_tweets(num_tweets)
+    print(tweets)    
+
+

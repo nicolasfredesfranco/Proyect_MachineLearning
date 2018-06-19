dab9e532495dbfde18138dec54a8a47e17da5622
opitz94 <38772626+opitz94@users.noreply.github.com>
2018-06-19 08:55:09 -0400

    sin disminuir dimension

--- ploting.py
+++ ploting.py
@@ -6,24 +6,20 @@ Created on Mon Jun 18 22:20:25 2018
 """
 
 from gensim.models import Word2Vec
-from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from sklearn.cluster import SpectralClustering
 
 
-modelo = Word2Vec.load("modelo")
-ts = TSNE(2)
+modelo = Word2Vec.load("modelo2d")
 lista = list(modelo.wv.vocab.keys())
 
-    
-    
-reduced_vecs = ts.fit_transform(modelo.wv[modelo.wv.vocab.keys()])
-spectral = SpectralClustering(n_clusters = 7, eigen_solver='arpack', affinity="nearest_neighbors")
+
+spectral = SpectralClustering(n_clusters = 3, eigen_solver='arpack', affinity="nearest_neighbors")
 spectral.fit(modelo.wv[modelo.wv.vocab.keys()])
 label = spectral.labels_
 
 
-for i in range(len(reduced_vecs)):
+for i in range(len(label)):
     if(label[i]==0):
         c = 'k'
     elif(label[i]==1):
@@ -33,11 +29,17 @@ for i in range(len(reduced_vecs)):
     elif(label[i]==3):
         c = 'b'
     elif(label[i]==4):
-        c = 'r'
-    elif(label[i]==5):
-        c = 'g'
-    elif(label[i]==6):
         c = 'y'
-    plt.plot(reduced_vecs[i,0],reduced_vecs[i,1],marker = 'x',color = c, markersize = 8)
+    else:
+        continue
+    plt.plot(modelo.wv[lista[i]][0],modelo.wv[lista[i]][1],marker = 'x',color = c, markersize = 5)
+plt.plot(modelo.wv['amor'][0],modelo.wv['amor'][1], marker = 'o', color = 'g', markersize = 8)
+plt.plot(modelo.wv['feliz'][0],modelo.wv['feliz'][1], marker = 'o', color = 'g', markersize = 8)
+plt.plot(modelo.wv['enamorado'][0],modelo.wv['enamorado'][1], marker = 'o', color = 'g', markersize = 8)
+plt.plot(modelo.wv['pena'][0],modelo.wv['pena'][1], marker = 'o', color = 'r', markersize = 8)
+plt.plot(modelo.wv['ira'][0],modelo.wv['ira'][1], marker = 'o', color = 'r', markersize = 8)
+plt.plot(modelo.wv['rabia'][0],modelo.wv['rabia'][1], marker = 'o', color = 'r', markersize = 8)
+plt.plot(modelo.wv['tiempo'][0],modelo.wv['tiempo'][1], marker = 'o', color = 'b', markersize = 8)
 plt.show()
-
+print(modelo.wv[lista[0]][0])
+print(modelo.wv[modelo.wv.vocab.keys()])

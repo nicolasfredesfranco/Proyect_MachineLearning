c602ce94f13ecd8020f8c29ed55875db84e2ba9e
facundolazcano <39739600+facundolazcano@users.noreply.github.com>
2018-06-18 22:12:32 -0400

    cambios tweet4.py ppsing.py
    Modifique ppsing y agregue una nueva funcion del ppsing q es igual processing pero no afecta el diccionario, este se ocupa cuando no se entrena, modifique Tweet4.py para que descargue solo los ultimos, al principio es 1.

--- ppsing.py
+++ ppsing.py
@@ -4,30 +4,36 @@ import unicodedata
 import pickle
 
 
-abc={'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m',' ','.'}#,'1','2','3','4','5','6','7','8','9'}
+abc={'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','ñ','m',' ','.'}#,'1','2','3','4','5','6','7','8','9'}
 
 stop_words=['de', 'la', 'que', 'el', 'en', 'y', 'a', 'los', 'del', 'se', 'las', 'por', 'un', 'para', 'con', 'no', 'una', 'su', 'al', 'lo', 'como', 'mas', 'pero', 'sus', 'le', 'ya', 'o', 'este', 'si', 'porque', 'esta', 'entre', 'cuando', 'muy', 'sin', 'sobre', 'tambien', 'me', 'hasta', 'hay', 'donde', 'quien', 'desde', 'todo', 'nos', 'durante', 'todos', 'uno', 'les', 'ni', 'contra', 'otros', 'ese', 'eso', 'ante', 'ellos', 'e', 'esto', 'mi', 'antes', 'algunos', 'que', 'unos', 'yo', 'otro', 'otras', 'otra', 'el', 'tanto', 'esa', 'estos', 'mucho', 'quienes', 'nada', 'muchos', 'cual', 'poco', 'ella', 'estar', 'estas', 'algunas', 'algo', 'nosotros', 'mi', 'mis', 'tu', 'te', 'ti', 'tu', 'tus', 'ellas', 'nosotras', 'vosostros', 'vosostras', 'os', 'mio', 'mia', 'mios', 'mias', 'tuyo', 'tuya', 'tuyos', 'tuyas', 'suyo', 'suya', 'suyos', 'suyas', 'nuestro', 'nuestra', 'nuestros', 'nuestras', 'vuestro', 'vuestra', 'vuestros', 'vuestras', 'esos', 'esas', 'estoy', 'estas', 'esta', 'estamos', 'estais', 'estan', 'este', 'estes', 'estemos', 'esteis', 'esten', 'estare', 'estaras', 'estara', 'estaremos', 'estareis', 'estaran', 'estaria', 'estarias', 'estariamos', 'estariais', 'estarian', 'estaba', 'estabas', 'estabamos', 'estabais', 'estaban', 'estuve', 'estuviste', 'estuvo', 'estuvimos', 'estuvisteis', 'estuvieron', 'estuviera', 'estuvieras', 'estuvieramos', 'estuvierais', 'estuvieran', 'estuviese', 'estuvieses', 'estuviesemos', 'estuvieseis', 'estuviesen', 'estando', 'estado', 'estada', 'estados', 'estadas', 'estad', 'he', 'has', 'ha', 'hemos', 'habeis', 'han', 'haya', 'hayas', 'hayamos', 'hayais', 'hayan', 'habre', 'habras', 'habra', 'habremos', 'habreis', 'habran', 'habria', 'habrias', 'habriamos', 'habriais', 'habrian', 'habia', 'habias', 'habiamos', 'habiais', 'habian', 'hube', 'hubiste', 'hubo', 'hubimos', 'hubisteis', 'hubieron', 'hubiera', 'hubieras', 'hubieramos', 'hubierais', 'hubieran', 'hubiese', 'hubieses', 'hubiesemos', 'hubieseis', 'hubiesen', 'habiendo', 'habido', 'habida', 'habidos', 'habidas', 'soy', 'eres', 'es', 'somos', 'sois', 'son', 'sea', 'seas', 'seamos', 'seais', 'sean', 'sere', 'seras', 'sera', 'seremos', 'sereis', 'seran', 'seria', 'serias', 'seriamos', 'seriais', 'serian', 'era', 'eras', 'eramos', 'erais', 'eran', 'fui', 'fuiste', 'fue', 'fuimos', 'fuisteis', 'fueron', 'fuera', 'fueras', 'fueramos', 'fuerais', 'fueran', 'fuese', 'fueses', 'fuesemos', 'fueseis', 'fuesen', 'sintiendo', 'sentido', 'sentida', 'sentidos', 'sentidas', 'siente', 'sentid', 'tengo', 'tienes', 'tiene', 'tenemos', 'teneis', 'tienen', 'tenga', 'tengas', 'tengamos', 'tengais', 'tengan', 'tendre', 'tendras', 'tendra', 'tendremos', 'tendreis', 'tendran', 'tendria', 'tendrias', 'tendriamos', 'tendriais', 'tendrian', 'tenia', 'tenias', 'teniamos', 'teniais', 'tenian', 'tuve', 'tuviste', 'tuvo', 'tuvimos', 'tuvisteis', 'tuvieron', 'tuviera', 'tuvieras', 'tuvieramos', 'tuvierais', 'tuvieran', 'tuviese', 'tuvieses', 'tuviesemos', 'tuvieseis', 'tuviesen', 'teniendo', 'tenido', 'tenida', 'tenidos', 'tenidas', 'tened','asi','q','d']
 
-#Se cargar el diccionario anterior, debe estar archivo "FrecWords" o sino habra error
 
-infile = open('FrecWords','rb')
-frec_words = pickle.load(infile)
-infile.close()
 
+#se carga el diccionario si no existe crea uno nuevo
+name_dict='dic1'
+frec_words=dict()
+try:
+    infile = open(name_dict,'rb')
+    frec_words = pickle.load(infile)
+    infile.close() 
+except FileNotFoundError:
+    frec_words=dict()
 
 #esta funcion elmina todos los signos que no son palabras
 def elimina_sign(text):
-    for i in range(len(text)-1):
-        if text[i] not in abc:
-             text=text[:i] + ' ' +text[i + 1:]
-    if text[len(text)-1] not in abc:
-        text=text[:len(text)-1]
-    return text
+    text_out=''
+    for i in text:
+        if i not in abc:
+             text_out+=' '
+        else :
+            text_out +=i
+    return text_out
 
 def elimina_tildes(s):
    return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
 
-
+## text es un string que deseas preprocesar, modificas el diccionario.
 def processing(text):
     output=list()        
     
@@ -52,25 +58,63 @@ def processing(text):
             words_sw = word_tokenize(fr)
             words=[]
             for i in words_sw:
-                if i not in stop_words:
-                    words.append(i)
+                if (len(i)>1):
+                    if (i not in stop_words):
+                                           
+                        words.append(i)
                     
-                    if i not in frec_words.keys():
-                        frec_words[i]=1;
-                    else:
-                        frec_words[i]=frec_words[i]+1
+                        if i not in frec_words.keys():
+                            frec_words[i]=1;
+                        else:
+                            frec_words[i]=frec_words[i]+1
+            if len(words)!=0:            
+                output.append(words)
 
+    return output
+
+#mismo codigo para preporsseing esta version se corre cuando el modelo ya esta creado.
+def ptext(text):
+    output=list()        
+    
+    #quita el enlace
+    text=text.split('https')[0]
+
+    #pasa todo a minusculas
+    text = text.lower()
+
+    #elmina signos de puntuacion
+    text=elimina_tildes(text)
+
+    #elimina caracteres
+    text=elimina_sign(text)
+
+    #separa el texto por frases u oraciones
+    frases=text.split('.');
+
+    #separa por las palabras
+    for fr in frases:
+        if len(fr)!=0: 
+            words_sw = word_tokenize(fr)
+            words=[]
+            for i in words_sw:
+                
+                if i in frec_words.keys():
+                    words.append(i)
 
             if len(words)!=0:            
                 output.append(words)
 
     return output
 
+
+
+
+
 def save_dict():
-    filename = 'FrecWords'
-    outfile = open(filename,'wb')
+    outfile = open(name_dict,'wb')
     pickle.dump(frec_words,outfile)
     outfile.close()
+
 
 
 
---
+++ tweet4.py
@@ -0,0 +1,114 @@
+import tweepy
+import ppsing #modulos de pre procesado
+from gensim.models import Word2Vec
+from time import sleep
+
+consumer_key = "YG9t6aMkiK3rvGdSbIkNbVKoG"
+consumer_secret = "xhyivSyaesMQ2PvJdceI7BwBX3sjXxzhHsvC2pZcV0i2VTZ16R"
+access_key = "879488168525004801-2wqBqwGmQzUNbvZLC1Oja7dyOF0ztLK"
+access_secret = "ajMqqwF7TBf2tQZpLQyNZfe9jSLh8K6USWJHjXkp1Ohwg"
+
+#configurar api
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+#ubicar chile    
+places = api.geo_search(query="CHILE", granularity="country")
+place_id = places[0].id
+
+
+def get_all_tweets(cant_tweets,oldest):
+
+    print('bajando...\n')
+    #salida, lista de listas de frases
+    alltweets = list()    
+
+
+    while (True):
+        #Leer datos de tweets
+        try:    
+            new_tweets = api.search(q="place:%s" % place_id,tweet_mode='extended',since_id=oldest)   
+        except :
+            break
+        
+        
+        if len(new_tweets) != 0:
+
+            #guarda el id del ultimo
+            oldest=new_tweets[-1].id-1 
+            
+            #por cada iteracion agrega las frases nuevas del tweets
+            for tweet in new_tweets:
+                alltweets.extend(ppsing.processing(tweet.full_text))         
+            #vacio lista
+            new_tweets=[]
+
+        #duerme durante 1 minuto
+        if (len(alltweets)>=cant_tweets):
+            break
+        else: 
+            sleep(60)        
+   
+    return alltweets,oldest
+
+
+def save_data (tweets,oldest):
+
+    
+    #Leer datos de tweets
+    try:    
+        infile = open('dataTweets2','rb')
+        data_tweet = ppsing.pickle.load(infile)
+        infile.close()   
+    except FileNotFoundError:
+        data_tweet=list()
+
+    data_tweet.extend(tweets)
+    outfile = open('dataTweets2','wb')
+    ppsing.pickle.dump(data_tweet,outfile)
+    outfile.close()
+
+    outfile = open('oldest','wb')
+    ppsing.pickle.dump(oldest,outfile)
+    outfile.close()
+
+    print('guardado\n')
+
+
+#main
+if __name__ == '__main__':
+
+
+    #indica cantidad de tweets en qu se bajara
+    cant_tweets=int(input('cantidad de frases:'))   
+    largo = 0
+    try:
+        infile = open('oldest','rb')
+        oldest= ppsing.pickle.load(infile)
+        infile.close()
+    except:
+        oldest=1
+
+    
+    while True:
+       
+        tweets,oldest=get_all_tweets(cant_tweets-largo,oldest)
+        len_tweets=len(tweets)
+
+        if (len_tweets != 0):
+            save_data(tweets,oldest)
+            ppsing.save_dict()
+            largo += len(tweets)
+
+        print(largo)
+
+        if (largo >= cant_tweets):
+            break
+        else:
+            print('dormir...')
+            sleep(16*60); # duerme 16 minutos
+
+    print('fin\n')
+
+
